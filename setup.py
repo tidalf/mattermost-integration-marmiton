6050a58e088bc39c600c39c0c901fc41d16e32ea
tidalf <tidalf@ematome.com>
2018-10-08 01:43:44 +0200

    docker

--- setup.py
+++ setup.py
@@ -1,45 +1,16 @@
-import os
-import subprocess
-from setuptools import setup
-from setuptools import find_packages
+#!/usr/bin/env python
 
-
-def version():
-    if not os.path.isdir(".git"):
-        print "This does not appear to be a Git repository."
-        return
-    try:
-        p = subprocess.Popen(["git", "describe",
-                              "--tags", "--always"],
-                             stdout=subprocess.PIPE)
-    except EnvironmentError:
-        print "unable to run git, leaving ecdsa/_version.py alone"
-        return
-    stdout = p.communicate()[0]
-    if p.returncode != 0:
-        print "unable to run git, leaving ecdsa/_version.py alone"
-        return
-    return stdout[:-1]
-
-
-README = os.path.join(os.path.dirname(__file__), 'README.md')
-
+from distutils.core import setup
 
 setup(
     name='mattermost_marmiton',
-    version=version(),
+    version='1.0.0',
     description="Marmiton Integration Service for Mattermost.",
-    long_description=README,
+    long_description="Marmiton Integration Service for Mattermost.",
     classifiers=[],
     author='d@corp',
     author_email='dakorp@ematome.com',
     url='https://github.com/tidalf/mattermost-integration-marmiton',
     license='',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'Flask==0.10.1',
-        'requests==2.10.0',
-    ]
+    packages=['mattermost_marmiton']
 )
