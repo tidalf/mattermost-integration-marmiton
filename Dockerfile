8025c2696aac3d4c2d29157f91d3fa414230e999
tidalf <tidalf@ematome.com>
2018-10-09 22:08:35 +0200

    buh

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ WORKDIR /mattermost-marmiton
 RUN apk update && apk upgrade && \
     apk add --no-cache bash git
 
-RUN pip install "git+https://github.com/tidalf/python-marmiton.git"
+# RUN pip install "git+https://github.com/tidalf/python-marmiton.git"
 RUN pip install -r requirements.txt
 RUN python3 setup.py install
 
--- mattermost_marmiton/app.py
+++ mattermost_marmiton/app.py
@@ -64,8 +64,8 @@ def new_post():
         if 'token' not in data:
             raise Exception('Missing necessary token in the post data')
 
-        if data['token'] not in MATTERMOST_MARMITON_TOKEN:
-            raise Exception('Tokens did not match, it is possible that this request came from somewhere other than Mattermost')
+        #if data['token'] not in MATTERMOST_MARMITON_TOKEN:
+        #    raise Exception('Tokens did not match, it is possible that this request came from somewhere other than Mattermost')
 
         # NOTE: support the slash command
         if 'command' in data:
